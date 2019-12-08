d641a0ba40f6100653927e79d619c7dc62ade770
Mazin Al Hinai <58527722+mazin44@users.noreply.github.com>
2019-12-07 23:31:11 -0700

    Add files via upload

--- Balloon/Balloon.m
+++ Balloon/Balloon.m
@@ -1,1 +1,12 @@
-
+function [tout,yout]=Balloon(ti,vi,zi,tf,dt,varargin)
+t = ti; y = [vi zi];
+tout = t; yout = y;
+while(1)
+    if t + dt > tf, dt = tf - t; end
+    dy=dydt(y,varargin{:});
+        y = y + dy' * dt;
+        t = t + dt;
+        tout = [tout; t]; yout = [yout; y];
+        if t >= tf, break, end
+    end
+end
