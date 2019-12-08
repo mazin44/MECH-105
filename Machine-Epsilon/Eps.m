e831e57ec4a048879141dcbc884737a392dfa73e
Mazin Al Hinai <58527722+mazin44@users.noreply.github.com>
2019-12-07 23:27:34 -0700

    Add files via upload

--- Machine-Epsilon/Eps.m
+++ Machine-Epsilon/Eps.m
@@ -1,1 +1,17 @@
+%Mazin Al Hinai
+%HW7
+%Machine eplsilon
+ep = 1;
+while(1)
+    
+    if 1+ep <= 1%Step=1
+       ep = 2*ep; %Step=5
+        break
+        
+    else 
+        ep = ep/2; %Step=3
+    end
+end
+ep
 
+eps
